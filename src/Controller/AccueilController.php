8a9e6614f9d6aa076f632ba3c5493f07fc6ed6eb
Craic-T <thomas.gardetto@gmail.com>
2022-07-27 16:02:06 +0200

    .env + debut front

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActiviteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(): Response
+    public function index(ActiviteRepository $activiteRepo ): Response
     {
+
+
         return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController'
+        ]);
+    }
+
+    #[Route('/activites', name: 'activites')]
+    public function activites(ActiviteRepository $activiteRepo ): Response
+    {
+        // VALEURS DE TEST
+        $test = $activiteRepo->findAll();
+        $ville = 'Bordeaux'; 
+        $meteo = 'ciel dégagé';
+
+        return $this->render('accueil/activites.html.twig', [
             'controller_name' => 'AccueilController',
+            'test'            => $test,
+            'ville'           => $ville,
+            'meteo'           => $meteo
         ]);
     }
 }
