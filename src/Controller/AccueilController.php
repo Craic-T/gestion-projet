a3194d947e7d432939e74e35fbc3f264ec6470c3
TontonGak0 <57990311+TontonGak0@users.noreply.github.com>
2022-07-28 10:45:17 +0200

    API + Recherche
    Intégration de l'api et du boolean pour check si activité intérieur ou exterieur

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -23,22 +23,35 @@ class AccueilController extends AbstractController
         if($form->isSubmitted() && $form->isValid())
         {
             $ville = $form['ville']->getData();
+          
 
             // API
+            /*  $apicall = file_get_contents("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&CIVIL&product=astro&output=json"); */
+
+            $apikey = "a0742c85528ef8c3c99498b98e9e20e7";
+            $apicall = "https://api.openweathermap.org/data/2.5/weather?q=$ville,fr&APPID=$apikey&units=metric&lang=fr";
+           $resultapi = json_decode(file_get_contents("$apicall"),true);
+         $keys = array_keys($resultapi); 
+           /* print_r($keys);
+           var_dump($resultapi["weather"][0]["main"]); */
             // requête API avec $ville
+            $temperature = $resultapi["main"]["temp"]; // temp récup depuis API
+            $condiMeteo = $resultapi["weather"][0]["main"]; // conditions météo de l'api
 
-            $temperature = 12; // temp récup depuis API
-            $condiMeteo = 'nuageux'; // conditions météo de l'api
-
-            if($temperature >= 15 && $condiMeteo == 'nuageux')
+            if($temperature >= 15 && $condiMeteo == 'Clear' || $condiMeteo == 'few clouds' || $condiMeteo == 'scattered clouds')
             {
+                $condiMeteo = $resultapi["weather"][0]["description"];
+                $resultapi["weather"][0]["main"];
                 $location = "ext";
-
-            }else
+            }
+            else
             {
+                $condiMeteo = $resultapi["weather"][0]["description"];
+                $resultapi["weather"][0]["main"];
                 $location = 'int';
             }
 
+            
             $activites = $activiteRepo->findBy(['location' => "$location"],);
 
             return $this->render('accueil/activites.html.twig', [
@@ -46,16 +59,25 @@ class AccueilController extends AbstractController
                 'test'            => $activites,
                 'ville'           => $ville,
                 'meteo'           => $condiMeteo,
-                'location'        => $location
+                'location'        => $location,
+                'apicall'          => $apicall,
+                'resultapi'        => $resultapi
+
             ]);
 
         }
+       
+       
 
 
 
+
+       
+        
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'form'            => $form->createView(),
+
         ]);
     }
 
@@ -72,7 +94,7 @@ class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
             'test'            => $test,
             'ville'           => $ville,
-            'meteo'           => $meteo
+            'meteo'           => $meteo,
         ]);
     }
 }
